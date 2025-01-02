e75ce5f939680463bb729b6a9bd1d323a1d28662
Zac Richards <107489668+ZacRichards@users.noreply.github.com>
2025-01-02 17:34:45 +0800

    make building containter quicker

--- var/docker/docker-build.sh
+++ var/docker/docker-build.sh
@@ -4,4 +4,3 @@ set -o xtrace
 
 docker rmi localhost/postiz || true
 docker build --target dist -t localhost/postiz -f Dockerfile.dev .
-docker build --target devcontainer -t localhost/postiz-devcontainer -f Dockerfile.dev .
